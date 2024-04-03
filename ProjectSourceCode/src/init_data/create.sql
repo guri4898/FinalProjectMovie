9ccf97a2c0e8cdab7917ff6ee5487fb6065b939d
Sergio Lopez <selo3582@colorado.edu>
2024-04-03 14:08:02 -0600

    fixing create.sql

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -12,12 +12,6 @@ CREATE TABLE users (
 --     password VARCHAR(255) NOT NULL,
 --     email VARCHAR(255) NOT NULL,
 -- );
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
-);
 
 -- DROP TABLE IF EXISTS movie CASCADE;
 -- CREATE TABLE movie (
