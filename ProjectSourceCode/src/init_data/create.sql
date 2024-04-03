f22a201c40588ef664419183ba72925dd2c3f843
Andrew Huynh <andrewhuynh1289@gmail.com>
2024-04-03 14:16:01 -0600

    fixed syntax

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,43 +1,39 @@
+DROP TABLE IF EXISTS users CASCADE;
 
-CREATE TABLE users (
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
--- DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS movie CASCADE;
 
--- CREATE TABLE users(
---     id SERIAL PRIMARY KEY,
---     username VARCHAR(255) NOT NULL,
---     password VARCHAR(255) NOT NULL,
---     email VARCHAR(255) NOT NULL,
--- );
+CREATE TABLE movie (
+    movie_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    year INTEGER NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    director VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    avg_rating INTEGER NOT NULL
+);
 
--- DROP TABLE IF EXISTS movie CASCADE;
--- CREATE TABLE movie (
---     id SERIAL PRIMARY KEY,
---     title VARCHAR(255) NOT NULL,
---     year INTEGER NOT NULL,
---     genre VARCHAR(255) NOT NULL,
---     director VARCHAR(255) NOT NULL,
---     description TEXT NOT NULL,
---     image VARCHAR(255) NOT NULL,
---     avg_rating INTEGER NOT NULL
--- );
+DROP TABLE IF EXISTS reviews CASCADE;
 
--- DROP TABLE IF EXISTS reviews CASCADE;
--- CREATE TABLE reviews (
---     id SERIAL PRIMARY KEY,
---     FOREIGN KEY movie_id REFERENCES movie (id),
---     FOREIGN KEY user_id REFERENCES users (id),
---     review TEXT NOT NULL,
--- );
+CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    FOREIGN KEY movie_id REFERENCES movie(movie_id),
+    FOREIGN KEY user_id REFERENCES users(user_id),
+    review TEXT NOT NULL
+);
 
--- DROP TABLE IF EXISTS rating CASCADE;
--- CREATE TABLE rating(
---     id SERIAL PRIMARY KEY,
---     FOREIGN KEY movie_id REFERENCES movie (id),
---     FOREIGN KEY user_id REFERENCES users (id),
---     rating INTEGER NOT NULL,
--- );
+DROP TABLE IF EXISTS rating CASCADE;
+CREATE TABLE rating(
+    rating_id SERIAL PRIMARY KEY,
+    FOREIGN KEY movie_id REFERENCES movie(movie_id),
+    FOREIGN KEY user_id REFERENCES users(user_id),
+    rating INTEGER NOT NULL
+);
 
